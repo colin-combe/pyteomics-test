e5f64a83059e8b8ebd35b32060e7024ae67ea256
colin combe <colin.combe@ed.ac.uk>
2024-05-13 08:51:51 +0100

    test with multi spec per match file

--- test_iterfind_when.py
+++ test_iterfind_when.py
@@ -30,22 +30,28 @@ def iterfind_when(source, target_name, condition_name, stack_predicate, **kwargs
     """
     g = etree.iterparse(source, ("start", "end"))
     state = False
+    history = []
     for event, tag in g:
         lc_name = _local_name(tag)
         if event == "start":
             if lc_name == condition_name:
                 state = stack_predicate(tag)
-            if lc_name == target_name and state:
-                yield source._get_info_smart(tag, **kwargs)
-            else:
-                tag.clear()
         else:
-            tag.clear()
+            if lc_name == target_name and state:
+                value = source._get_info_smart(tag, **kwargs)
+                for t in history:
+                    t.clear()
+                history.clear()
+                yield value
+            elif state:
+                history.append(tag)
+            elif not state:
+                tag.clear()
 
 
 # @profile
 def test3():
-    reader = mzid.MzIdentML("test2.mzid", retrieve_refs=False, use_index=True)
+    reader = mzid.MzIdentML("multiple_spectra_per_id_1_3_0_draft.mzid", retrieve_refs=False, use_index=True)
     sil_ids = list(reader.index['SpectrumIdentificationList'].keys())
     for sil_id in sil_ids:
         print("Processing SpectrumIdentificationList", sil_id)
@@ -53,21 +59,22 @@ def test3():
         # print(sir)
         reader.reset()
         for e in iterfind_when(
-            reader,
-            "SpectrumIdentificationResult",
-            "SpectrumIdentificationList",
-            lambda x: x.attrib["id"] == sil_id,
-            retrieve_refs=False
+                reader,
+                "SpectrumIdentificationResult",
+                "SpectrumIdentificationList",
+                lambda x: x.attrib["id"] == sil_id,
+                retrieve_refs=False
         ):
             try:
-               e['SpectrumIdentificationItem']
-               # print(f"Has SpectrumIdentificationItem?: {e}")
+                e['SpectrumIdentificationItem']
+                print(f"Has SpectrumIdentificationItem?: {e['SpectrumIdentificationItem']}")
             except KeyError:
-                print (f"Has no SpectrumIdentificationItem?: {e}")
+                print(f"Has no SpectrumIdentificationItem?: {e}")
         pass
     print("Test 3 done!")
 
-print ("Test 3 - iter when")
+
+print("Test 3 - iter when")
 # log timing info
 start = time.time()
 test3()
